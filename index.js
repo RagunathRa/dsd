b3ca541200687a0da9d356a641f61786706a690d
ragunathr <ragunathr@tenacitics.in>
2024-11-21 23:40:49 +0530

    test1

---
+++ index.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-unused-vars */
+function sayHello(){
+    const x = 8;
+    console.log("Hello, World!", x);
+    alert("Hello Everyone..! ")
+  }
+  /* eslint-enable no-unused-vars */
+  
